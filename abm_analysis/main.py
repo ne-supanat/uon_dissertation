065005268a08ee92739d234712205c38a4a48822
ne-supanat <naesup@gmail.com>
2025-07-06 11:48:58 +0100

    - updated main process (devided process into 3 parts 1. key components, 2. profiles, 3. simulation script)

--- abm_analysis/main.py
+++ abm_analysis/main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 import llm
 
@@ -20,114 +21,145 @@ def get_transcript_file_paths(source_path):
     # return [f"{source_path}/{filename}" for filename in sorted(os.listdir(source_path))]
 
 
-def main():
-    folder_path = "abm_analysis/results"
-    os.makedirs(folder_path, exist_ok=True)
+def main(objective, model_input, model_output, questions):
+    source_paths = get_transcript_file_paths("data/diary_txt")
+    results_path = "abm_analysis/results"
+    os.makedirs(results_path, exist_ok=True)
 
-    # Objective, Input, Output
-    objective = "explore different usages of transportation from home to workplace"
-    input = "traveller characteristic (transportation preference)"
-    output = "number of used of each transportation type"
+    step = int(
+        input(
+            """Steps:
+1 - Generate EABSS Key components (scope, diagrams)
+2 - Generate Profiles and action probability table
+3 - Generate Simulation script (NetLogo)
 
-    document_paths = get_transcript_file_paths("data/diary_txt")
-
-    ta_codes_txt_path = folder_path + "/thematic_analysis_codes.txt"
-    ta_codes_csv_path = folder_path + "/thematic_analysis_codes.csv"
-
-    # # Extract key components codes with supporting quotes
-    # thematic_analysis.analyse(document_paths, ta_codes_txt_path, ta_codes_csv_path)
-    # thematic_analysis_evaluation.evaluate(ta_codes_csv_path)
-    # thematic_analysis_extra.analyse(document_paths)
-
-    kc_scope_path = folder_path + "/key_component_scope.txt"
-    kc_usecase_diagram_path = folder_path + "/key_component_usecase_diagram.txt"
-    kc_activity_diagram_path = folder_path + "/key_component_activity_diagram.txt"
-    kc_state_transition_diagram_path = (
-        folder_path + "/key_component_state_transition_diagram.txt"
+Select step: """
+        )
     )
-    kc_interaction_diagram_path = folder_path + "/key_component_interaction_diagram.txt"
 
-    # # Generate ABM key components
-    # key_component_generation.generate(
-    #     objective,
-    #     input,
-    #     output,
-    #     ta_codes_txt_path,
-    #     kc_scope_path,
-    #     kc_usecase_diagram_path,
-    #     kc_activity_diagram_path,
-    #     kc_state_transition_diagram_path,
-    #     kc_interaction_diagram_path,
-    # )
-    # # + human review: codes and quotes coherence, diagrams review
-    # # * UPDATE archetype model *
+    ta_codes_txt_path = results_path + "/thematic_analysis_codes.txt"
+    ta_codes_csv_path = results_path + "/thematic_analysis_codes.csv"
 
-    profiles_path = folder_path + "/profiles.txt"
+    if step == 1:
+        # Extract key components codes with supporting quotes
+        thematic_analysis.analyse(source_paths, ta_codes_txt_path, ta_codes_csv_path)
+        thematic_analysis_evaluation.evaluate(ta_codes_csv_path)
+        thematic_analysis_extra.analyse(source_paths)
 
-    # # Profiles generation
-    # profile_generation.generate(
-    #     document_paths,
-    #     objective,
-    #     kc_scope_path,
-    #     profiles_path,
-    # )
-    # profile_evaluation.evaluate(profiles_path)
-    # # + human review: attribute correctness and archetype and quotes coherence
-
-    scenario_questions_path = folder_path + "/scenario_questions.txt"
-    scenario_ground_truth_path = folder_path + "/scenario_ground_truth.txt"
-
-    scenario_answers_path = folder_path + "/scenario_answers.csv"
-    scenario_scores_path = folder_path + "/scenario_scores.csv"
-
-    # Scenario-question create
-    with open(scenario_questions_path, "w") as f:
-        f.write(
-            """[
-"Which transport mode do you usually take?",
-"Which transport mode do you take when it rain?"
-]"""
+        kc_scope_path = results_path + "/key_component_scope.txt"
+        kc_usecase_diagram_path = results_path + "/key_component_usecase_diagram.txt"
+        kc_activity_diagram_path = results_path + "/key_component_activity_diagram.txt"
+        kc_state_transition_diagram_path = (
+            results_path + "/key_component_state_transition_diagram.txt"
+        )
+        kc_interaction_diagram_path = (
+            results_path + "/key_component_interaction_diagram.txt"
         )
 
-    # # Scenario-question answering
-    # scenario_decision.generate_profile_answers(
-    #     scenario_questions_path,
-    #     profiles_path,
-    #     scenario_answers_path,
-    # )
-    # scenario_decision_evaluation.generate_ground_truth(
-    #     scenario_questions_path,
-    #     scenario_ground_truth_path,
-    # )
-    # scenario_decision_evaluation.score_profile_anwsers(
-    #     scenario_ground_truth_path,
-    #     scenario_answers_path,
-    #     scenario_scores_path,
-    # )
+        # Generate ABM key components
+        key_component_generation.generate(
+            objective,
+            model_input,
+            model_output,
+            ta_codes_txt_path,
+            kc_scope_path,
+            kc_usecase_diagram_path,
+            kc_activity_diagram_path,
+            kc_state_transition_diagram_path,
+            kc_interaction_diagram_path,
+        )
 
-    scenario_probability_path = folder_path + "/scenario_probability.csv"
+        # + human review: codes and quotes coherence, diagrams review
+        print("- Done -")
+        print(
+            """Please review generated results and update "Archetype model" and "Scenario questions\""""
+        )
 
-    # Decision table
-    decision_table.generate(
-        scenario_questions_path, scenario_answers_path, scenario_probability_path
-    )
+    profiles_path = results_path + "/profiles.txt"
 
-    simulation_script_path = folder_path + "/simulation_script.txt"
+    if step == 2:
+        pass
+        # Profiles generation
+        profile_generation.generate(
+            source_paths,
+            objective,
+            kc_scope_path,
+            profiles_path,
+        )
+        profile_evaluation.evaluate(profiles_path)
+        # + human review: attribute correctness and archetype and quotes coherence
 
-    # Generate simulation script
-    script_generation.generate(
-        objective,
-        input,
-        output,
-        kc_scope_path,
-        kc_usecase_diagram_path,
-        kc_activity_diagram_path,
-        kc_state_transition_diagram_path,
-        kc_interaction_diagram_path,
-        scenario_probability_path,
-        simulation_script_path,
-    )
+    scenario_questions_path = results_path + "/scenario_questions.txt"
+    scenario_ground_truth_path = results_path + "/scenario_ground_truth.txt"
+
+    scenario_answers_path = results_path + "/scenario_answers.csv"
+    scenario_scores_path = results_path + "/scenario_scores.csv"
+
+    if step == 2:
+        pass
+        # Scenario-question create
+        with open(scenario_questions_path, "w") as f:
+            f.write(questions)
+
+        # Scenario-question answering
+        scenario_decision.generate_profile_answers(
+            scenario_questions_path,
+            profiles_path,
+            scenario_answers_path,
+        )
+        scenario_decision_evaluation.generate_ground_truth(
+            scenario_questions_path,
+            scenario_ground_truth_path,
+        )
+        scenario_decision_evaluation.score_profile_anwsers(
+            scenario_ground_truth_path,
+            scenario_answers_path,
+            scenario_scores_path,
+        )
+
+    scenario_probability_path = results_path + "/scenario_probability.csv"
+
+    if step == 2:
+        pass
+        # Decision table
+        decision_table.generate(
+            scenario_questions_path, scenario_answers_path, scenario_probability_path
+        )
+
+        print("- Done -")
+
+    simulation_script_path = results_path + "/simulation_script.txt"
+
+    if step == 3:
+        # Generate simulation script
+        script_generation.generate(
+            objective,
+            model_input,
+            model_output,
+            kc_scope_path,
+            kc_usecase_diagram_path,
+            kc_activity_diagram_path,
+            kc_state_transition_diagram_path,
+            kc_interaction_diagram_path,
+            scenario_probability_path,
+            simulation_script_path,
+        )
+
+        print("- Done -")
+        print(
+            """Please review generated script. Further edit might required (ChatGPT can help fix the script)"""
+        )
 
 
 if __name__ == "__main__":
-    main()
+    # Objective, Input, Output
+    objective = "explore different usages of transportation from home to workplace"
+    model_input = "traveller characteristic (transportation preference)"
+    model_output = "number of used of each transportation type"
+
+    questions = """[
+"Which transport mode do you usually take?",
+"Which transport mode do you take when it rain?"
+]"""
+
+    main(objective, model_input, model_output, questions)
