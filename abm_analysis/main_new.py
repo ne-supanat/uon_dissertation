1ef4a11b35bb5fc1b4b04802f90db7d5f30afd6b
ne-supanat <naesup@gmail.com>
2025-07-14 18:25:29 +0100

    - updated main flow

--- abm_analysis/main_new.py
+++ abm_analysis/main_new.py
@@ -2,12 +2,14 @@ import os
 import sys
 import json
 
-import problem_statement_definition
+import abm_analysis.problem_statement_setup as problem_statement_setup
 
 import thematic_analysis
 import thematic_analysis_evaluation
 import thematic_analysis_extra
 import key_component_generation
+
+import archetype_scenario_setup
 
 import profile_generation
 import profile_evaluation as profile_evaluation
@@ -24,134 +26,19 @@ def get_transcript_file_paths(source_path):
     # return [f"{source_path}/{filename}" for filename in sorted(os.listdir(source_path))]
 
 
-def main_0(objective, model_input, model_output, questions):
-    source_paths = get_transcript_file_paths("data/diary_txt")
-    results_path = "abm_analysis/results"
-    os.makedirs(results_path, exist_ok=True)
+def ask_approval(stage_str: str) -> bool:
+    approve = None
+    while approve not in ["y", "n"]:
+        approve = input(f"Proceed to next stage: {stage_str}? (y/n): ").lower()
+    return approve == "y"
 
-    step = int(
-        input(
-            """Steps:
-1 - Generate EABSS Key components (scope, diagrams)
-2 - Generate Profiles and action probability table
-3 - Generate Simulation script (NetLogo)
 
-Select step: """
-        )
-    )
-
-    ta_codes_txt_path = results_path + "/thematic_analysis_codes.txt"
-    ta_codes_csv_path = results_path + "/thematic_analysis_codes.csv"
-
-    if step == 1:
-        # Extract key components codes with supporting quotes
-        thematic_analysis.analyse(source_paths, ta_codes_txt_path, ta_codes_csv_path)
-        thematic_analysis_evaluation.evaluate(ta_codes_csv_path)
-        thematic_analysis_extra.analyse(source_paths)
-
-        kc_scope_path = results_path + "/key_component_scope.txt"
-        kc_usecase_diagram_path = results_path + "/key_component_usecase_diagram.txt"
-        kc_activity_diagram_path = results_path + "/key_component_activity_diagram.txt"
-        kc_state_transition_diagram_path = (
-            results_path + "/key_component_state_transition_diagram.txt"
-        )
-        kc_interaction_diagram_path = (
-            results_path + "/key_component_interaction_diagram.txt"
-        )
-
-        # Generate ABM key components
-        key_component_generation.generate(
-            objective,
-            model_input,
-            model_output,
-            ta_codes_txt_path,
-            kc_scope_path,
-            kc_usecase_diagram_path,
-            kc_activity_diagram_path,
-            kc_state_transition_diagram_path,
-            kc_interaction_diagram_path,
-        )
-
-        # + human review: codes and quotes coherence, diagrams review
+def print_end_stage(stage_str: str, is_last_stage: bool = False):
+    print(f"\nStage: {stage_str} complete.")
+    if not is_last_stage:
+        print("Please run 'python main.py' to continue to the next step.")
+    else:
         print("- Done -")
-        print(
-            """Please review generated results and update "Archetype model" and "Scenario questions\""""
-        )
-
-    profiles_path = results_path + "/profiles.txt"
-
-    if step == 2:
-        pass
-        # Profiles generation
-        profile_generation.generate(
-            source_paths,
-            objective,
-            kc_scope_path,
-            profiles_path,
-        )
-        profile_evaluation.evaluate(profiles_path)
-        # + human review: attribute correctness and archetype and quotes coherence
-
-    scenario_questions_path = results_path + "/scenario_questions.txt"
-    scenario_ground_truth_path = results_path + "/scenario_ground_truth.txt"
-
-    scenario_answers_path = results_path + "/scenario_answers.csv"
-    scenario_scores_path = results_path + "/scenario_scores.csv"
-
-    if step == 2:
-        pass
-        # Scenario-question create
-        with open(scenario_questions_path, "w") as f:
-            f.write(questions)
-
-        # Scenario-question answering
-        scenario_decision.generate_profile_answers(
-            scenario_questions_path,
-            profiles_path,
-            scenario_answers_path,
-        )
-        scenario_decision_evaluation.generate_ground_truth(
-            scenario_questions_path,
-            scenario_ground_truth_path,
-        )
-        scenario_decision_evaluation.score_profile_anwsers(
-            scenario_ground_truth_path,
-            scenario_answers_path,
-            scenario_scores_path,
-        )
-
-    scenario_probability_path = results_path + "/scenario_probability.csv"
-
-    if step == 2:
-        pass
-        # Decision table
-        decision_table.generate(
-            scenario_questions_path, scenario_answers_path, scenario_probability_path
-        )
-
-        print("- Done -")
-
-    simulation_script_path = results_path + "/simulation_script.txt"
-
-    if step == 3:
-        # Generate simulation script
-        script_generation.generate(
-            objective,
-            model_input,
-            model_output,
-            kc_scope_path,
-            kc_usecase_diagram_path,
-            kc_activity_diagram_path,
-            kc_state_transition_diagram_path,
-            kc_interaction_diagram_path,
-            scenario_probability_path,
-            simulation_script_path,
-        )
-
-        print("- Done -")
-        print(
-            """Please review generated script. Further edit might required (ChatGPT can help fix the script)"""
-        )
 
 
 def main(source_folder: str, results_folder: str):
@@ -159,100 +46,198 @@ def main(source_folder: str, results_folder: str):
     results_path = results_folder
     os.makedirs(results_path, exist_ok=True)
 
-    ## Define Problem statement, Input * Ouput
-    problem_statement_path = results_path + "/problem_statement.txt"
-    # TODO: state problem
-
-    ## Build EABSS components
-    # Thematic analysis
-    ta_codes_txt_path = results_path + "/thematic_analysis_codes.txt"
-    # TODO: conduct thematic analyse
-
-    # Finalise EABSS
-    EABSS_components_path = results_path + "/eabss_scope.txt"
-    EABSS_usecase_diagram_path = results_path + "/eabss_diagram.txt"
-    EABSS_activity_diagram_path = results_path + "/eabss_activity_diagram.txt"
-    EABSS_state_transition_diagram_path = results_path + "/eabss_state_diagram.txt"
-    # TODO: add transition table = results_path + "/eabss_state_table.txt"
-    EABSS_interaction_diagram_path = results_path + "/eabss_interaction_diagram.txt"
-    # TODO: finalise EABSS components
-    # TODO: tell user to review & update EABSS components
-    # TODO: tell user to update relevant models for next step (archetypes, questions, choices)
-
-    ## Define scenario-questions & answer choices
-    # Define scenario questions
-    # TODO:
-    # Define scenario answer choices
-    # TODO:
-
-    ## Extract Profiles (& classify profile archetype)
-    # Extract profile
-    profiles_path = results_path + "/profiles.txt"
-    # TODO:
-
-    # Answer scenario-questions
-    scenario_questions_path = results_path + "/scenario_questions.txt"
-    scenario_answers_path = results_path + "/scenario_answers.csv"
-    # TODO:
-
-    # # Profile Evaluation
-    # scenario_ground_truth_path = results_path + "/scenario_ground_truth.txt"
-    # scenario_scores_path = results_path + "/scenario_scores.csv"
-    # # TODO: extract profiles
-
-    ## Create Decision probability table
-    decision_probability_path = results_path + "/scenario_probability.csv"
-    # TODO: create table
-
-    ## Generate Simulation script
-    simulation_script_path = results_path + "/simulation_script.txt"
-    # TODO: generate simulation script
-    # TODO: tell user to review & update script (manually/LLMs)
-
-    # //
-
     # NOTE: if main file of that stage exists that process already done
     # if not os.path.isfile(problem_statement_path):
     # TODO: (optional) change to check project name and its stage in json
+    # TODO: make user approve the stage result first
+
+    ## Define Problem statement, Input & Ouput
+    problem_statement_path = os.path.join(results_path, "problem_statement.txt")
+
     if not os.path.isfile(problem_statement_path):
+        # New project
         print("\nNo existing project detected.")
         print("Starting new project...")
-        problem_statement_definition.define_problem_statement(problem_statement_path)
-        print("Stage 1 complete.")
-        print("Please run 'python main.py' to continue to the next step.")
+        problem_statement_setup.define_problem_statement(problem_statement_path)
+        display_progress.display_problem_statement(problem_statement_path)
+        print_end_stage("Define Problem statement, Input & Output")
         sys.exit()
+    else:
+        # Display problem statement result
+        display_progress.display_header()
+        display_progress.display_problem_statement(problem_statement_path)
 
-    if not os.path.isfile(EABSS_components_path):
-        display_progress.display(problem_statement_path)
+    ## Build EABSS components
+    ta_codes_txt_path = os.path.join(results_path, "thematic_analysis_codes.txt")
+    ta_codes_csv_path = os.path.join(results_path, "thematic_analysis_codes.csv")
 
-        # TODO: implement build eabss
-        print("Stage 2 complete.")
+    eabss_components_path = os.path.join(results_path, "eabss_scope.txt")
+
+    if not os.path.isfile(eabss_components_path):
+        stage_str = "Build EABSS components"
+        proceed = ask_approval(stage_str)
+
+        if proceed:
+            # # Thematic analysis
+            # thematic_analysis.analyse(
+            #     source_paths,
+            #     ta_codes_txt_path,
+            #     ta_codes_csv_path,
+            # )
+
+            # # Finalise EABSS components
+            # key_component_generation.generate_components(
+            #     problem_statement_path,
+            #     ta_codes_txt_path,
+            #     eabss_components_path,
+            # )
+
+            # # EABSS components Evaluation
+            # thematic_analysis_evaluation.evaluate(ta_codes_csv_path)
+            # thematic_analysis_extra.analyse(source_paths)
+
+            display_progress.display_eabss_components(eabss_components_path)
+            print_end_stage(stage_str)
+
         sys.exit()
+    else:
+        # Display EABSS components
+        display_progress.display_eabss_components(eabss_components_path)
 
-    if not os.path.isfile(scenario_questions_path) and not os.path.isfile(
-        scenario_answers_path
+    ## Build EABSS diagrams
+    eabss_usecase_diagram_path = os.path.join(results_path, "eabss_usecase_diagram.txt")
+    eabss_activity_diagram_path = os.path.join(
+        results_path, "eabss_activity_diagram.txt"
+    )
+    eabss_state_transition_diagram_path = os.path.join(
+        results_path, "eabss_state_diagram.txt"
+    )
+    # TODO: add transition table = os.path.join(results_path,"/eabss_state_table.txt")
+    eabss_interaction_diagram_path = os.path.join(
+        results_path, "eabss_interaction_diagram.txt"
+    )
+
+    if not os.path.isfile(eabss_usecase_diagram_path):
+        stage_str = "Generate EABSS diagrams"
+        proceed = ask_approval(stage_str)
+        if proceed:
+            # Generate EABSS diagrams
+            key_component_generation.generate_diagrams(
+                eabss_components_path,
+                eabss_usecase_diagram_path,
+                eabss_activity_diagram_path,
+                eabss_state_transition_diagram_path,
+                eabss_interaction_diagram_path,
+            )
+
+            display_progress.display_eabss_diagrams(
+                eabss_usecase_diagram_path,
+                eabss_activity_diagram_path,
+                eabss_state_transition_diagram_path,
+                eabss_interaction_diagram_path,
+            )
+
+            print_end_stage(stage_str)
+        sys.exit()
+    else:
+        # Display EABSS diagrams
+        display_progress.display_eabss_diagrams(
+            eabss_usecase_diagram_path,
+            eabss_activity_diagram_path,
+            eabss_state_transition_diagram_path,
+            eabss_interaction_diagram_path,
+        )
+
+    ## Define scenario-questions & answer choices
+    archetype_path = os.path.join(results_path, "archetype.py")
+    scenario_questions_path = os.path.join(results_path, "scenario_questions.txt")
+    scenario_answer_choices_path = os.path.join(results_path, "answers_choices.py")
+
+    if (
+        not os.path.isfile(archetype_path)
+        or not os.path.isfile(scenario_questions_path)
+        or not os.path.isfile(scenario_answer_choices_path)
     ):
+        stage_str = "Define Archetype, Scenario questions & answer choices"
+        proceed = ask_approval(stage_str)
+        archetype_scenario_setup.setup_archetype_scenario()
+        # TODO: tell user to update relevant models for next step (archetypes, questions, choices)
         print("- scenario questions step")
-        # TODO: back to complete previous step or continue
-        # TODO: remind update scenario questions & answers
-        pass
-        sys.exit()
-    if not os.path.isfile(profiles_path):
-        print("- extract profiles step")
-        # TODO: back to complete previous step or continue
-        # TODO: implement extract profile
-        pass
-    if not os.path.isfile(decision_probability_path):
-        print("- decision prob step")
-        # TODO: implement create decision table
-        pass
-    if not os.path.isfile(simulation_script_path):
-        print("- sim script step")
-        # TODO: implement generate script
-        pass
+        # Define scenario questions
+        # TODO:
+        # with open(scenario_questions_path, "w") as f:
+        #     f.write("questions")
+        # Define scenario answer choices
+        # TODO:
 
-    print("- Done -")
-    sys.exit()
+        print_end_stage(stage_str)
+        sys.exit()
+
+    # ## Extract Profiles (& classify profile archetype)
+    # profiles_path = os.path.join(results_path, "profiles.txt")
+    # scenario_answers_path = os.path.join(results_path, "scenario_answers.csv")
+
+    # if not os.path.isfile(profiles_path):
+    #     print("- extract profiles step")
+    #     # Extract profile
+    #     profile_generation.generate(
+    #         source_paths,
+    #         problem_statement_path,
+    #         eabss_components_path,
+    #         profiles_path,
+    #     )
+    #     # Answer scenario-questions
+    #     scenario_decision.generate_profile_answers(
+    #         scenario_questions_path,
+    #         profiles_path,
+    #         scenario_answers_path,
+    #     )
+    #     # # Profile Evaluation
+    #     # scenario_ground_truth_path = os.path.join(results_path, "scenario_ground_truth.txt")
+    #     # scenario_scores_path = os.path.join(results_path, "scenario_scores.csv")
+    #     # profile_evaluation.evaluate(profiles_path)
+    #     # scenario_decision_evaluation.generate_ground_truth(
+    #     #     scenario_questions_path,
+    #     #     scenario_ground_truth_path,
+    #     # )
+    #     # scenario_decision_evaluation.score_profile_anwsers(
+    #     #     scenario_ground_truth_path,
+    #     #     scenario_answers_path,
+    #     #     scenario_scores_path,
+    #     # )
+
+    #     print_end_stage(4)
+    #     sys.exit()
+
+    # ## Create Decision probability table
+    # decision_probability_path = os.path.join(results_path, "scenario_probability.csv")
+    # if not os.path.isfile(decision_probability_path):
+    #     print("- decision prob step")
+    #     decision_table.generate(
+    #         scenario_questions_path, scenario_answers_path, decision_probability_path
+    #     )
+    #     print_end_stage(5)
+    #     sys.exit()
+
+    # ## Generate Simulation script
+    # simulation_script_path = os.path.join(results_path, "simulation_script.txt")
+
+    # if not os.path.isfile(simulation_script_path):
+    #     print("- sim script step")
+    #     script_generation.generate(
+    #         problem_statement_path,
+    #         eabss_components_path,
+    #         eabss_usecase_diagram_path,
+    #         eabss_activity_diagram_path,
+    #         eabss_state_transition_diagram_path,
+    #         eabss_interaction_diagram_path,
+    #         decision_probability_path,
+    #         simulation_script_path,
+    #     )
+    #     # TODO: tell user to review & update script (manually/LLMs)
+
+    #     print_end_stage(6)
+    #     sys.exit()
 
 
 if __name__ == "__main__":
