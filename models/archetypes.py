7baff0010379957427d20a983931c6b96848c980
ne-supanat <naesup@gmail.com>
2025-08-01 01:46:33 +0100

    - updated result files

--- models/archetypes.py
+++ models/archetypes.py
@@ -1,11 +1,5 @@
 import enum
 
-
 class Archetype(enum.Enum):
-    CarOwner = "Car owner"
-    BusPassHolder = "Bus pass holder"
-    Students = "Students"
-    Commuter = "Commuter"
-    CarDependent = "Car dependent"
-    PublicTransportUser = "Public transport user"
-    EnvironmentallyConsciousCommuter = "Environmentally Conscious Commuter"
+	Environmentalists = "environmentalists"
+	NonEnvironmentalists = "non-environmentalists"
--- models/scenario_choices.py
+++ models/scenario_choices.py
@@ -1,6 +1,5 @@
 import enum
 
 class ScenarioChoice(enum.Enum):
-	Buses = "Buses"
-	Trains = "Trains"
 	Cars = "Cars"
+	PublicTransport = "Public transport"
